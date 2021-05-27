8429debd5c6c4d61f7e54db080b3d38cd768ddad
Mr.Kiwi <83403872+KiwiLifeIsGood@users.noreply.github.com>
2021-05-27 19:14:39 +0700

    Add files via upload

---
+++ PRO1/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PRO1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            LoadDataGridView();
+        }
+
+         private DataTable dataTable = new DataTable();
+        void LoadDataGridView()
+        {
+            dataTable.Columns.Add("Ho va Ten", typeof(String));
+            dataTable.Columns.Add("Biet Danh", typeof(int));
+            dataTable.Columns.Add("Tuoi", typeof(int));
+            
+
+            dataGridView1.DataSource = dataTable;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AddSV_Click(object sender, EventArgs e)
+        {
+            dataTable.Rows.Add(textBox1.Text, textBox2.Text, textBox3.Text);
+            dataGridView1.DataSource = dataTable;
+        }
+    }
+}
